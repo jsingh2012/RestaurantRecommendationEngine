d5d788a658c6eacee605a663dcd3a56e91470829
jatinderpal singh <jatinderpals@capitalsource.in>
2023-04-13 13:12:11 +0530

    Added restaurants Api

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 
 // server.js File
 const express = require('express'); // Importing express module
+const date = require('date-and-time')
 const app = express(); // Creating an express object
 app.use(express.json());
 const port = 8100;  // Setting an port for this application
@@ -17,15 +18,16 @@ app.listen(port, function (err) {
 })
 
 Cuisines = []
+Restaurant = []
 
 app.get("/cuisines/", function(req, res) {
     res.end( JSON.stringify(Cuisines))
 })
 
 app.post("/cuisines/", function(req, res) {
-    console.log(req.body);
+    // console.log(req.body);
     const {data: newCuisines} = req.body
-    console.log(newCuisines);
+    // console.log(newCuisines);
     listOfnewCuisines = newCuisines.split(",")
     listOfnewCuisines.forEach(element => {
         element = element.trim()
@@ -35,4 +37,19 @@ app.post("/cuisines/", function(req, res) {
     })
     
     res.end( JSON.stringify(Cuisines))
+})
+
+app.get("/restaurants/", function(req, res) {
+    res.end( JSON.stringify(Restaurant))
+})
+
+app.post("/restaurants/", function(req, res) {
+    //console.log(req.body);
+    //req.body.id = length(Restaurant)+1
+    data = {...req.body, id:Restaurant.length+1 }
+    //console.log(data);
+    //const now  =  new Date();
+    //console.log(date.format(now,'DD/MM/YYYY'))
+    Restaurant = [...Restaurant, data]
+    res.end( JSON.stringify(Restaurant))
 })
